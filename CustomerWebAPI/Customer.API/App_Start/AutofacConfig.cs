f0147b16d96dca6872a7f798a2fc661fd36b1ec3
Amit P Naik <amit.naik8103@gmail.com>
2020-04-25 13:46:43 +0530

    setting up with ef

---
+++ CustomerWebAPI/Customer.API/App_Start/AutofacConfig.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using Customer.API.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace Customer.API.App_Start
+{
+    public class AutofacConfig
+    {
+        public static void Register()
+        {
+            var bldr = new ContainerBuilder();
+            var config = GlobalConfiguration.Configuration;
+            bldr.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            RegisterServices(bldr);
+            bldr.RegisterWebApiFilterProvider(config);
+            bldr.RegisterWebApiModelBinderProvider();
+            var container = bldr.Build();
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static void RegisterServices(ContainerBuilder bldr)
+        {
+            bldr.RegisterType<CampContext>()
+              .InstancePerRequest();
+
+            bldr.RegisterType<CampRepository>()
+              .As<ICampRepository>()
+              .InstancePerRequest();
+        }
+    }
+}
---
+++ CustomerWebAPI/Customer.API/Data/ICampRepository.cs
@@ -0,0 +1,36 @@
+﻿using Customer.API.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customer.API.Data
+{
+    public interface ICampRepository
+    {
+        // General 
+        Task<bool> SaveChangesAsync();
+
+        // Camps
+        void AddCamp(Camp camp);
+        void DeleteCamp(Camp camp);
+        Task<Camp[]> GetAllCampsAsync(bool includeTalks = false);
+        Task<Camp> GetCampAsync(string moniker, bool includeTalks = false);
+        Task<Camp[]> GetAllCampsByEventDate(DateTime dateTime, bool includeTalks = false);
+
+        // Talks
+        void AddTalk(Talk talk);
+        void DeleteTalk(Talk talk);
+        Task<Talk> GetTalkByMonikerAsync(string moniker, int talkId, bool includeSpeakers = false);
+        Task<Talk[]> GetTalksByMonikerAsync(string moniker, bool includeSpeakers = false);
+
+        // Speakers
+        void AddSpeaker(Speaker speaker);
+        void DeleteSpeaker(Speaker speaker);
+        Task<Speaker[]> GetSpeakersByMonikerAsync(string moniker);
+        Task<Speaker> GetSpeakerAsync(int speakerId);
+        Task<Speaker[]> GetAllSpeakersAsync();
+
+    }
+}
